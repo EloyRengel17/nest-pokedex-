6e3b9ee72390f1c8e86bb831832035effce2f644
EloyRengel17 <eloyrengel17@gmail.com>
2026-01-24 12:03:46 -0400

    first commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsNumber, IsPositive, Min, IsString, MinLength } from "class-validator"
+export class CreatePokemonDto {
+
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
+    no:number
+
+    @IsString()
+    @MinLength(1)
+    name:string
+}
